8d6172d43b6150dd48f384191f87909403172e2f
JParrales <josedparrales@gmail.com>
2020-04-15 15:37:08 -0500

    string copy

---
+++ _strcmp.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * _strcmp - compares two strings.
+ * @s1: Destiny
+ * @s2: source
+ * Return: Always 0.
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+	for (i = 0; s1[i] != '\0'; i++)
+	{
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+	}
+	return (0);
+}
