fd57a462c799aae91ae1953ea7fb320783958687
JParrales <josedparrales@gmail.com>
2020-04-15 15:49:57 -0500

    sting len

---
+++ _strlen.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * _strlen - returns the length of a string.
+ * @s: Pointer
+ * Return: Always 0.
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != 0; i++)
+	{
+	}
+	return (i);
+}
